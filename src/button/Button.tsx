b019abdeb4a48cd0623e9d622e5e38da741b1992
那可心 <nakx@raysdata.com>
2022-08-31 09:56:07 +0800

    Eslint + Prettier + Husky

--- src/button/Button.tsx
+++ src/button/Button.tsx
@@ -1,51 +1,60 @@
-import { defineComponent, PropType, toRefs } from 'vue';
+import { defineComponent, PropType } from "vue";
 // import 'uno.css';
 
-export type ISize = 'small' | 'medium' | 'large';
-export type IColor = 'black' | 'gray' | 'red' | 'yellow' | 'green' | 'blue' | 'indigo' | 'purple' | 'pink';
+export type ISize = "small" | "medium" | "large";
+export type IColor =
+  | "black"
+  | "gray"
+  | "red"
+  | "yellow"
+  | "green"
+  | "blue"
+  | "indigo"
+  | "purple"
+  | "pink";
 export const props = {
   size: {
     type: String as PropType<ISize>,
-    default: 'medium'
+    default: "medium",
   },
   color: {
     type: String as PropType<IColor>,
-    default: 'blue'
+    default: "blue",
   },
   round: {
     type: Boolean,
-    default: false
+    default: false,
   },
   plain: {
     type: Boolean,
-    default: false
+    default: false,
   },
   icon: {
     type: String,
-    default: ''
-  }
+    default: "",
+  },
 } as const;
 
 export default defineComponent({
-  name: 'SButton',
+  name: "SButton",
   props,
   setup(props, { slots }) {
     const size = {
       small: {
-        x: '2',
-        y: '1',
-        text: 'sm'
+        x: "2",
+        y: "1",
+        text: "sm",
       },
       medium: {
-        x: '3',
-        y: '1.5',
-        text: 'base'
+        x: "3",
+        y: "1.5",
+        text: "base",
       },
       large: {
-        x: '4',
-        y: '2',
-        text: 'lg'
-      }
+        x: "4",
+        y: "2",
+        text: "lg",
+      },
     };
 
     return () => (
@@ -53,21 +62,26 @@ export default defineComponent({
         class={`
         py-${size[props.size].y} 
         px-${size[props.size].x} 
-        ${props.round ? 'rounded-full' : 'rounded-lg'} 
-        bg-${props.color}-${props.plain ? '100' : '500'} 
+        ${props.round ? "rounded-full" : "rounded-lg"} 
+        bg-${props.color}-${props.plain ? "100" : "500"} 
         hover:bg-${props.color}-400 
-        border-${props.color}-${props.plain ? '500' : '500'} 
+        border-${props.color}-${props.plain ? "500" : "500"} 
         border-1 
-        text-${props.plain ? props.color + '-500' : 'white'} 
+        text-${props.plain ? props.color + "-500" : "white"} 
         text-${size[props.size].text} 
         hover:text-white 
         cursor-pointer 
         transition duration-300 ease-in-out transform hover:scale-105 
         mx-1
-    `}>
-        {props.icon !== '' ? <i class={`i-ic-baseline-${props.icon} p-3`}></i> : ''}
-        {slots.default ? slots.default() : ''}
+    `}
+      >
+        {props.icon !== "" ? (
+          <i class={`i-ic-baseline-${props.icon} p-3`}></i>
+        ) : (
+          ""
+        )}
+        {slots.default ? slots.default() : ""}
       </button>
     );
-  }
+  },
 });
--- src/button/__tests__/Button.spec.ts
+++ src/button/__tests__/Button.spec.ts
@@ -1,40 +1,45 @@
 import Button from "../Button";
 
-import { shallowMount } from '@vue/test-utils';
-import { describe, expect, test } from 'vitest'
+import { shallowMount } from "@vue/test-utils";
+import { describe, expect, test } from "vitest";
 
-describe('Button', () => {
-  test('mount @vue/test-utils', () => {
+describe("Button", () => {
+  test("mount @vue/test-utils", () => {
     const wrapper = shallowMount(Button, {
       slots: {
-        default: 'Button'
-      }
-    })
-    expect(wrapper.text()).toBe('Button')
-  })
-})
+        default: "Button",
+      },
+    });
+    expect(wrapper.text()).toBe("Button");
+  });
+});
 
-describe('color', () => {
-  test('default', () => {
+describe("color", () => {
+  test("default", () => {
     const wrapper = shallowMount(Button, {
       slots: {
-        default: 'Button'
-      }
-    })
+        default: "Button",
+      },
+    });
 
-    expect(wrapper.classes().map(v => v.replace('\n', '')).includes('bg-blue-500')).toBe(true)
-  })
+    expect(
+      wrapper
+        .classes()
+        .map((v) => v.replace("\n", ""))
+        .includes("bg-blue-500")
+    ).toBe(true);
+  });
 
-  test('red', () => {
+  test("red", () => {
     const wrapper = shallowMount(Button, {
       slots: {
-        default: 'Button'
+        default: "Button",
       },
       props: {
-        color: 'red'
-      }
-    })
+        color: "red",
+      },
+    });
 
-    expect(wrapper.classes().includes('bg-red-500')).toBe(true)
-  })
-})
+    expect(wrapper.classes().includes("bg-red-500")).toBe(true);
+  });
+});
