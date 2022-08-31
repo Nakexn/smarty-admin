92c55a9f0933112e1d084be55523c014aa32bd6a
那可心 <nakx@raysdata.com>
2022-09-01 00:20:27 +0800

    refactor: monorepo

--- packages/smarty-ui-vite/vite.config.ts
+++ packages/smarty-ui-vite/vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import Unocss from './config/unocss'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import Unocss from "./config/unocss";
 
 // https://vitejs.dev/config/
 
@@ -9,34 +9,34 @@ const rollupOptions = {
   external: ["vue"],
   output: {
     globals: {
-      vue: 'Vue'
+      vue: "Vue",
     },
   },
-}
+};
 
 export default defineConfig({
   plugins: [vue(), vueJsx({}), Unocss()],
   build: {
     rollupOptions,
-    minify: 'terser', // boolean | 'terser' | 'esbuild'
+    minify: "terser", // boolean | 'terser' | 'esbuild'
     sourcemap: true, // 输出单独 source文件
-    brotliSize: true,  // 生成压缩大小报告
+    brotliSize: true, // 生成压缩大小报告
     cssCodeSplit: true,
     lib: {
-      entry: './src/entry.ts',
-      name: 'SmartyUI',
-      fileName: 'smarty-ui',
+      entry: "./src/entry.ts",
+      name: "SmartyUI",
+      fileName: "smarty-ui",
       // 导出模块格式 推荐使用esm
       // @ts-ignore
       formats: ["esm", "umd", "iife"],
-    }
+    },
   },
   test: {
     // enable jest-like global test APIs
     globals: true,
-    environment: 'happy-dom',
+    environment: "happy-dom",
     transformMode: {
-      web: [/.[tj]sx$/]
-    }
-  }
-})
+      web: [/.[tj]sx$/],
+    },
+  },
+});
